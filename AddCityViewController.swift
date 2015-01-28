a16e7f71d97b08def7c2ac3351d97b309a80fd74
Mike Reining <emailmike@gmail.com>
2015-01-27 21:57:54 -0800

    Add city - added some checking into UITextFields to make sure that users are entering the correct input.

--- AddCityViewController.swift
+++ AddCityViewController.swift
@@ -19,7 +19,9 @@ class AddCityViewController: UIViewController, UITextFieldDelegate {
         
         title = "Add New City"
         self.view.backgroundColor = UIColor.purpleColor()
-
+        city = City(name: "", temperature: "", icon: "")
+        
+        
         // Add City Name Text Field
         cityNameTextField = UITextField(frame: CGRectMake(20.0, 30.0, 300, 33.0))
         cityNameTextField.center = CGPointMake(170, 150)
@@ -51,17 +53,47 @@ class AddCityViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(textField: UITextField!) -> Bool {   //delegate method
         if textField == cityNameTextField {
+            if cityNameTextField.text == "" {
+                let alert = UIAlertController(title: "Please Add City Name", message: "The name of the city cannot be blank!", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "Ops.", style: .Default, handler: nil)
+                alert.addAction(action)
+                presentViewController(alert, animated: true, completion: nil)
+            }
+            city!.name = cityNameTextField.text
             cityNameTextField.resignFirstResponder()
             temperatureTextField.becomeFirstResponder()
             return true
         }
         
         if textField == temperatureTextField {
+            
+            let letters = NSCharacterSet.letterCharacterSet()
+            let digits = NSCharacterSet.decimalDigitCharacterSet()
+            
+            var letterCount = 0
+            var digitCount = 0
+            
+            for uni in temperatureTextField.text.unicodeScalars {
+                if letters.longCharacterIsMember(uni.value) {
+                    letterCount++
+                } else if digits.longCharacterIsMember(uni.value) {
+                    digitCount++
+                }
+            }
+            
+            if digitCount == 0 && letterCount > 1 {
+                let alert = UIAlertController(title: "Invalid Input", message: "Please add a valid temperature as an integer value", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "Ok fine.", style: .Default, handler: nil)
+                alert.addAction(action)
+                presentViewController(alert, animated: true, completion: nil)
+            }
+            city!.temperature = temperatureTextField.text
             temperatureTextField.resignFirstResponder()
             iconTextField.becomeFirstResponder()
         }
         
         if textField == iconTextField {
+            city!.icon = iconTextField.text.lowercaseString
             iconTextField.resignFirstResponder()
             addCity(textField)
         }
@@ -71,9 +103,11 @@ class AddCityViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    func addCity(sender:UITextField!)
-    {
-        println("addCity Function called")
+    func addCity(sender:UITextField!) {
+        let alert = UIAlertController(title: "New City Details", message: "City name: \(city!.name), Temperature: \(city!.temperature), Icon name: \(city!.icon).  Rest of Add function not implemented since sleep is important too!", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Ok, got it!", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
     }
     
 }
